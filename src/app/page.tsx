3c16773c45bd5ea41449ba2ec607fc58db223114
Prerak siddhpura <prerak.siddhpura@gmail.com>
2024-12-21 03:08:47 +0530

    ,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { index } from "drizzle-orm/mysql-core";
 import Link from "next/link";
 import { db } from "~/server/db";
 
-export const dynamic = "force-dynamic"
+// These two are enough to force dynamic behavior
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 const mockUrls = [
   "https://thumbs.dreamstime.com/b/chain-links-shown-sky-blue-background-made-up-many-small-frozen-time-concept-mystery-wonder-324071465.jpg",
@@ -17,25 +19,25 @@ const mockImages = mockUrls.map((url , index) => ({
 }))
 
 export default async function HomePage() {
-
-  const posts = await db.query.posts.findMany()
-  console.log(posts)
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
 
   const duplicatedImages = [
     ...mockImages.map(img => ({ ...img, id: img.id })),
     ...mockImages.map(img => ({ ...img, id: img.id + mockImages.length }))
   ];
+  
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
         {posts.map((post)=> (<div key={post.id}>{post.name}</div>))}
         {
-      duplicatedImages.map((image , index) => (
-        <div key={image.id + "-"  + index} className="w-48 p-3">
-            <img src={image.url} alt="" />
-        </div>
-      ))
-    }
+          duplicatedImages.map((image , index) => (
+            <div key={image.id + "-"  + index} className="w-48 p-3">
+                <img src={image.url} alt="" />
+            </div>
+          ))
+        }
       </div>
     </main>
   );
